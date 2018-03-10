261d668a3c531e2b1b0388866b9e52b70d4eb1fe
chenlin80s <37233571+chenlin80s@users.noreply.github.com>
2018-03-10 14:18:49 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyBiEy9sT43nqRsx9KxtC-r6qy_7pRYfHhI"));
+define('GJ_CODE', getenv("KR"));
+define('SITE_NAME',getenv("SCJ"));
+define('TITLENAME', getenv("SCJ"));
+define('EN2DEKEY',getenv(">[QNC6Sk>4'wchN,&z'oj_|lQhjZZ1"));
+define('EMAIL', getenv("chenlin80s@gmail.com"));
 ?>
