e4dc4a71e19f1cc6caf0d89a394ab16d43cbd2ce
chenlin80s <37233571+chenlin80s@users.noreply.github.com>
2018-03-10 14:20:10 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', getenv("AIzaSyBiEy9sT43nqRsx9KxtC-r6qy_7pRYfHhI"));
-define('GJ_CODE', getenv("KR"));
-define('SITE_NAME',getenv("SCJ"));
-define('TITLENAME', getenv("SCJ"));
-define('EN2DEKEY',getenv(">[QNC6Sk>4'wchN,&z'oj_|lQhjZZ1"));
-define('EMAIL', getenv("chenlin80s@gmail.com"));
-?>
